f85a4828bad65c5b7c9435fbfe0f469f8fb13192
Andrew Melis <andrewmelis@gmail.com>
2015-04-18 15:29:52 -0500

    add factorial implementation

--- lab0/lab0.clj
+++ lab0/lab0.clj
@@ -4,3 +4,7 @@
 
 (defn cube [n]
   (* n n n))
+
+(defn factorial [n]
+  {:pre [(> n 0)]}
+  (apply * (range 1 (inc n))))
--- lab0/lab0_test.clj
+++ lab0/lab0_test.clj
@@ -6,3 +6,9 @@
   (is (= 1000 (lab0/cube 10)))
   (is (= 1 (lab0/cube 1)))
   (is (= -125 (lab0/cube -5))))
+
+(deftest factorial-tests
+  (is (= 1 (lab0/factorial 1)))
+  (is (= 120 (lab0/factorial 5)))
+  (is (thrown? AssertionError (lab0/factorial 0)))
+  (is (thrown? AssertionError (lab0/factorial -1))))
